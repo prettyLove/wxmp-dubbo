18beff12c8e637ff8ed62cc4e243a2fa512edc30
master <574286814@qq.com>
2019-11-04 22:13:19 +0800

    临时提交

---
+++ wxmp-generator/src/main/java/com/kochun/wxmp/WxmpGenerator.java
@@ -0,0 +1,13 @@
+package com.kochun.wxmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxmpGenerator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxmpGenerator.class, args);
+    }
+
+}
