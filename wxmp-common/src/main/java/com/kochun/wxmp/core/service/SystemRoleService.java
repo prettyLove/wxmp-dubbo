2ef32fa8696057df2eeaa596365c3ef4dc9f125f
master <574286814@qq.com>
2020-01-06 21:29:10 +0800

    临时提交

--- wxmp-common/src/main/java/com/kochun/wxmp/core/service/SystemRoleService.java
+++ wxmp-common/src/main/java/com/kochun/wxmp/core/service/SystemRoleService.java
@@ -1,18 +1,36 @@
 package com.kochun.wxmp.core.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.kochun.wxmp.core.entity.system.SysUserRole;
+import com.kochun.wxmp.core.entity.system.Role;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 角色表 服务类
  * </p>
  *
  * @author kochun
- * @since 2019-08-22
+ * @since 2019-08-18
  */
-public interface SystemRoleService extends IService<SysUserRole> {
+public interface SystemRoleService extends IService<Role> {
+
+    /**
+     * @author kochun
+     * @description 通过userId获得该用户的所有角色ID
+     * @date 2019/8/22 13:46
+     * @param userId
+     * @return java.util.List<Role>
+     **/
+    List<Role> getRoleIdsByUserId(long userId);
 
 
-
+    /**
+     * @author kochun
+     * @description 通过userId获得该用户的所有角色ID
+     * @date 2019/8/22 13:46
+     * @param userName
+     * @return java.util.List<Role>
+     **/
+    List<Role> getRoleIdsByUserName(String userName);
 }
